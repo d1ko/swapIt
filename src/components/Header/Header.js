36c3fd0a38765ac0cdb90a2adefbfd10e7af40cd
d1ko <58879685+d1ko@users.noreply.github.com>
2023-06-28 18:47:51 +0600

    delete one

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,17 +8,12 @@ export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <Link to="/main">
+        <Link to="/">
           <img src="https://m.media-amazon.com/images/I/81isydLZhUL.png" alt="Logo" />
         </Link>
       </div>
       <nav className={styles.nav}>
         <ul>
-          <li>
-            <Button type="link" className={styles.button}>
-              <Link to="/products">Продукты</Link>
-            </Button>
-          </li>
           <li>
             <Button type="link" className={styles.button}>
               <Link to="/guide">Инструкция</Link>

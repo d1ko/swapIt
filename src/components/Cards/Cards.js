409e28dffdc581dda911ce36b04d7491effb00da
d1ko <58879685+d1ko@users.noreply.github.com>
2023-06-29 18:10:41 +0600

    add show more modal window

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Card, Button, Modal, Form, Input } from 'antd';
+import { Card, Button, Modal, Form, Input, Carousel } from 'antd';
 import styles from './styles.module.css';
 import heart from '../../assets/images/heart.png';
 import fullHeart from '../../assets/images/heart-full.png';
@@ -7,10 +7,11 @@ import fullHeart from '../../assets/images/heart-full.png';
 const { Meta } = Card;
 
 export const Cards = () => {
-  const [image, setImage] = useState(heart);
   const [cards, setCards] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
 
-  const api = process.env.REACT_APP_API
+  const api = process.env.REACT_APP_API;
 
   useEffect(() => {
     fetchCards();
@@ -20,14 +21,32 @@ export const Cards = () => {
     try {
       const response = await fetch(`${api}/api/v1/products`);
       const data = await response.json();
-      setCards(data.results);
+      // Добавляем начальное состояние лайка для каждой карточки
+      const cardsWithLikes = data.results.map((card) => ({
+        ...card,
+        liked: false,
+      }));
+      setCards(cardsWithLikes);
     } catch (error) {
       console.error('Error', error);
     }
   };
 
-  const handleClick = () => {
-    setImage((prevImage) => (prevImage === heart ? fullHeart : heart));
+  const handleClick = (card) => {
+    setSelectedCard(card);
+    setModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
+  const handleLike = (cardId) => {
+    setCards((prevCards) =>
+      prevCards.map((card) =>
+        card.id === cardId ? { ...card, liked: !card.liked } : card
+      )
+    );
   };
 
   return (
@@ -41,16 +60,44 @@ export const Cards = () => {
         >
           <Meta title={card.name} description={card.description} className={styles.cardB} />
           <div className={styles.buttonCul}>
-            <Button>show more</Button>
-            <img
-              alt="none"
-              src={image}
-              className={styles.cardImgLike}
-              onClick={handleClick}
-            />
+            <Button onClick={() => handleClick(card)}>Show More</Button>
+            {card.liked ? (
+              <img
+                alt="none"
+                src={fullHeart}
+                className={styles.cardImgLike}
+                onClick={() => handleLike(card.id)}
+              />
+            ) : (
+              <img
+                alt="none"
+                src={heart}
+                className={styles.cardImgLike}
+                onClick={() => handleLike(card.id)}
+              />
+            )}
           </div>
         </Card>
       ))}
+      {selectedCard && (
+        <Modal
+          visible={modalVisible}
+          onCancel={handleModalClose}
+          footer={null}
+          className={styles.modal}
+        >
+          <h2>{selectedCard.name}</h2>
+          <Carousel>
+            {selectedCard.images.map((image) => (
+              <div key={image.id}>
+                <img src={image.image} alt="example" className={styles.modalImg} />
+              </div>
+            ))}
+          </Carousel>
+          <p>{selectedCard.description}</p>
+          <Button type="primary">Exchange</Button>
+        </Modal>
+      )}
     </div>
   );
 };
